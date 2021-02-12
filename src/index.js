ede6a526d93834680ae4347489500fa9aebaadd9
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-02-12 16:17:54 +0700

    table list jquery is done

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import * as $ from 'jquery';
 import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+$('#trigger').hover(function(){
+    $('#list').fadeIn();
+});
+
+$('#trigger').mouseenter(function(){
+    $('#table').show();
+    $('#list').hide();
+});
+
+$('.element_1').mouseleave(function(){
+    $('#table').hide();
+    $('#list').show();
+
+});
